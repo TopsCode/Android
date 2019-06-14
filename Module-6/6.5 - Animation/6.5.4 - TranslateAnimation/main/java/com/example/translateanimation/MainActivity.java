ce990c6f8f0347956cceddab6948355f6617cc82
TopsCode <kailash.solanki@tops-int.com>
2019-06-14 18:49:04 +0530

    Add files via upload

---
+++ Module-6/6.5 - Animation/6.5.4 - TranslateAnimation/main/java/com/example/translateanimation/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.translateanimation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView=findViewById(R.id.imageView);
+    }
+
+    public void startAnimation(View view) {
+
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.translate);
+        imageView.startAnimation(animation);
+    }
+}
---
+++ Module-6/6.5 - Animation/6.5.5 - RotateAnimation/main/java/com/example/rotateanimation/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.rotateanimation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView=findViewById(R.id.imageView);
+    }
+
+    public void start(View view) {
+        Animation animation= AnimationUtils.loadAnimation(this,R.anim.rotate);
+        imageView.startAnimation(animation);
+    }
+}
